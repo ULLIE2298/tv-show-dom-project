bbeebad46ab07bc8e73e7aa380e8985ae9350673
Ulrich Ernstzen <uernstzen2298@gmail.com>
2021-07-05 13:15:47 +0200

    finished level 300

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // You can edit ALL of the code here
+let search = document.getElementById("search");
+let searchResult = document.getElementById("searchResult");
+let rootElem = document.getElementById("root");
+let allEpisodes = getAllEpisodes();
+let selector = document.getElementById("selector");
+let newList = [];
+let selectedOpt = [];
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  console.log(episodeList[0]);  
-
+function makePageForEpisodes(episodeList) { 
   for (let episode of episodeList) {
     let container = document.createElement("div");
     container.className = "column";
@@ -22,8 +25,6 @@ function makePageForEpisodes(episodeList) {
     let imgEl = document.createElement("img");
     let summaryP = document.createElement("p");
     summaryP.className = "summary";
-
-    let info = document.getElementById("info");
 
     nameEp.textContent = `${episode.name}`;
     imgEl.src = episode.image.medium;
@@ -43,5 +44,54 @@ function makePageForEpisodes(episodeList) {
     container.appendChild(summaryP);
   }
 }
+
+// selection bar options
+function addOption(episodeArray){
+  let displayAllEp = document.createElement("option");
+  selector.appendChild(displayAllEp);
+  displayAllEp.innerText = "Display all";
+
+  episodeArray.forEach(element => {
+    let optionElem = document.createElement("option");
+    selector.appendChild(optionElem);
+    if (element.season < 10 && element.number < 10) {
+      optionElem.textContent = `S0${element.season}E0${element.number} - ${element.name}`;
+    } else if (element.number < 11 && element.season < 10) {
+      optionElem.textContent = `S0${element.season}E${element.number} - ${element.name}`;
+    }
+  });
+}
+addOption(allEpisodes);
+
+// Selection bar output
+selector.addEventListener("click", () => {
+  let selection = selector.value;
+  if(selection === "Display all") {
+    rootElem.innerHTML = "";
+    makePageForEpisodes(allEpisodes);
+  } else {
+    selectedOpt = allEpisodes.filter(el => selection.includes(el.name));
+    rootElem.innerHTML = "";
+    makePageForEpisodes(selectedOpt);
+  }
+});
+
+// Search input
+search.addEventListener("input", () => {
+    let result = search.value.toLowerCase();
+    newList = allEpisodes.filter( (el) => {
+      return (
+            el.name.toLowerCase().includes(result) || 
+            el.summary.toLowerCase().includes(result)
+      );
+    });
+   
+    rootElem.innerHTML = "";
+    selector.innerHTML = "";
+    addOption(newList);
+    makePageForEpisodes(newList);
+
+    searchResult.innerText = `Displaying ${newList.length}/${allEpisodes.length}`;
+  });
 
 window.onload = setup;
